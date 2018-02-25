287282dd51ca31cbc3991a6abd763bf44bc6b3aa
root <root@vps484443.ovh.net>
2018-02-25 22:55:03 +0100

    Boucle sur les commandes; tubes non fonctionnels

--- fichiers_fournis/src/shell.c
+++ fichiers_fournis/src/shell.c
@@ -17,7 +17,7 @@ int main()
 {
 	while (1) {
 		struct cmdline *l;
-		int i, j;
+		int i;
 		pid_t pid;
 
 		printf("shell> ");
@@ -34,47 +34,44 @@ int main()
 			printf("error: %s\n", l->err);
 			continue;
 		}
-
-		pid = fork();
-		if(pid == 0){
-			if (l->in) {
-				int fdIn = open(l->in, O_RDONLY, 0);
-				dup2(fdIn, STDIN_FILENO);
-				close(fdIn);
-			}
-			
-			if (l->out){
-				int fdOut = open(l->out, O_WRONLY, 0);
-				
-				if(fdOut == -1){
-					fprintf(stderr, "%s: Permission denied\n", l->out);
-				}else{
-					dup2(fdOut, STDOUT_FILENO);
-				}
-
-				close(fdOut);
-			}
-
-			int execError;
-			char* command = l->seq[0][0];
-			execError = execvp(command, l->seq[0]);
-			if(execError == -1){
-				fprintf(stderr, "%s: command not found\n", command);
-			}
-
-			exit(0);
-		}else{
-			int status;
-			waitpid(pid, &status, 0);
-		}
+	
 
 		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			//printf("seq[%d]: ", i);
-			for (j=0; cmd[j]!=0; j++) {
-				//printf("%s ", cmd[j]);
+		for (i=0; l->seq[i]!=0; i++) {		
+			pid = fork();
+			//printf("pid : %d, itération : %d", getpid(), i);
+			if(pid == 0){
+				if (l->in) {
+					int fdIn = open(l->in, O_RDONLY, 0);
+					dup2(fdIn, STDIN_FILENO);
+					close(fdIn);
+				}
+				
+				if (l->out){
+					int fdOut = open(l->out, O_WRONLY, 0);
+					
+					if(fdOut == -1){
+						fprintf(stderr, "%s: Permission denied\n", l->out);
+					}else{
+						dup2(fdOut, STDOUT_FILENO);
+					}
+
+					close(fdOut);
+				}
+
+				int execError;
+				char* command = l->seq[i][0];
+				execError = execvp(command, l->seq[i]);
+				if(execError == -1){
+					fprintf(stderr, "%s: command not found\n", command);
+				}
+
+				exit(0);
+			}else{
+				int status;
+				waitpid(pid, &status, 0);
 			}
+
 			printf("\n");
 		}
 	}
