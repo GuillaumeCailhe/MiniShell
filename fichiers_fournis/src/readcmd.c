f22dcf6bee9504d25d29748bf903574a549ec262
root <root@localhost.localdomain>
2018-02-27 09:06:07 -0500

    question 8 : gestion &

--- fichiers_fournis/src/readcmd.c
+++ fichiers_fournis/src/readcmd.c
@@ -189,6 +189,7 @@ struct cmdline *readcmd(void)
 	s->in = 0;
 	s->out = 0;
 	s->seq = 0;
+	s->bg = 0;
 
 	i = 0;
 	while ((w = words[i++]) != 0) {
@@ -223,7 +224,10 @@ struct cmdline *readcmd(void)
 				s->err = "misplaced pipe";
 				goto error;
 			}
-
+			if (s->bg){
+				s->err = "misplaced &";
+				goto error;
+			}
 			seq = xrealloc(seq, (seq_len + 2) * sizeof(char **));
 			seq[seq_len++] = cmd;
 			seq[seq_len] = 0;
@@ -231,6 +235,14 @@ struct cmdline *readcmd(void)
 			cmd = xmalloc(sizeof(char *));
 			cmd[0] = 0;
 			cmd_len = 0;
+			break;
+		case '&':
+			/* Tricky : the word can only be "&" */
+			if (cmd_len == 0) {
+				s->err = "misplaced &";
+				goto error;
+			}
+			s->bg = 1;
 			break;
 		default:
 			cmd = xrealloc(cmd, (cmd_len + 2) * sizeof(char *));
